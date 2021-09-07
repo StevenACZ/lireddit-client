e295add6803169d90015be394373e12fbfe07ef5
stevenacz <scoailazaa@gmail.com>
2021-09-07 09:22:59 -0500

    🚚 register added

---
+++ src/pages/register.tsx
@@ -0,0 +1,58 @@
+// React
+import React from 'react';
+
+// Formik
+import { Form, Formik } from 'formik';
+
+// Chakra
+import { Box } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/button';
+
+// Components
+import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => {
+	return (
+		<Wrapper variant="small">
+			<Formik
+				initialValues={{ username: '', password: '' }}
+				onSubmit={values => {
+					console.log(values);
+				}}
+			>
+				{({ isSubmitting }) => (
+					<Form>
+						<InputField
+							name="username"
+							placeholder="username"
+							label="Username"
+							isLoading={isSubmitting}
+						/>
+						<Box mt={4}>
+							<InputField
+								name="password"
+								placeholder="password"
+								label="Password"
+								type="password"
+								isLoading={isSubmitting}
+							/>
+						</Box>
+						<Button
+							mt={4}
+							colorScheme="teal"
+							isLoading={isSubmitting}
+							type="submit"
+						>
+							Submit
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</Wrapper>
+	);
+};
+
+export default Register;
