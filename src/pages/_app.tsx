2543ec5ce6139acc10c94f2d4ab549ba00320203
stevenacz <scoailazaa@gmail.com>
2021-09-07 09:22:00 -0500

    🚚 _app added

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+// Chakra
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+
+// Theme
+import theme from '../theme';
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<ChakraProvider resetCSS theme={theme}>
+			<ColorModeProvider
+				options={{
+					useSystemColorMode: true,
+				}}
+			>
+				<Component {...pageProps} />
+			</ColorModeProvider>
+		</ChakraProvider>
+	);
+}
+
+export default MyApp;
