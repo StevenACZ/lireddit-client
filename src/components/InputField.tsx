50366108c74bcbc8e9cef5cdd59f5aaa1d42e550
stevenacz <scoailazaa@gmail.com>
2021-09-07 12:25:51 -0500

    🧰 improved to get props to Input

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -14,18 +14,21 @@ import { Input } from '@chakra-ui/input';
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
 	label: string;
-	placeholder: string;
 	name: string;
 	isLoading: boolean;
 };
 
-export const InputField: React.FC<InputFieldProps> = props => {
+export const InputField: React.FC<InputFieldProps> = ({
+	label,
+	size: _,
+	...props
+}) => {
 	const [field, { error }] = useField(props);
 
 	return (
 		<FormControl isInvalid={!!error} isDisabled={props.isLoading}>
-			<FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-			<Input {...field} id={field.name} placeholder={props.placeholder} />
+			<FormLabel htmlFor={field.name}>{label}</FormLabel>
+			<Input {...field} {...props} id={field.name} />
 			{error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
 		</FormControl>
 	);
